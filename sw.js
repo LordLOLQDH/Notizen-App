92ec6c553f0d123aa3edb5d0da4718b1dc0220ed
LordLOLQDH <adam_kraus@icloud.com>
2026-09-20 14:31:20 +0200

    Service Worker Cache auf v3 aktualisieren

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="notes-app-v2";
+const CACHE_NAME="notes-app-v3-20260920";
 const FILES=["./","./index.html","./app.js","./sw.js","./manifest.json","./icon-192.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
